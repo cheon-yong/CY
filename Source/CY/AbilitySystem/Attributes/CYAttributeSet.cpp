6c44c966c89c8a58f8969960b41d25da0222d17b
cheonyongjun <yes1312@naver.com>
2024-12-23 20:48:30 +0900

    데미지 Attribute 연동

--- Source/CY/AbilitySystem/Attributes/CYAttributeSet.cpp
+++ Source/CY/AbilitySystem/Attributes/CYAttributeSet.cpp
@@ -2,4 +2,72 @@
 
 
 #include "AbilitySystem/Attributes/CYAttributeSet.h"
+#include "GameplayEffectExtension.h"
 
+UCYAttributeSet::UCYAttributeSet() :
+	AttackRange(100.0f),
+	MaxAttackRange(300.0f),
+	AttackRadius(50.f),
+	MaxAttackRadius(150.0f),
+	AttackRate(30.0f),
+	MaxAttackRate(100.0f),
+	MaxHealth(100.0f),
+	Damage(0.0f)
+{
+	InitHealth(GetMaxHealth());
+}
+
+void UCYAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	if (Attribute == GetDamageAttribute())
+	{
+		NewValue = NewValue < 0.0f ? 0.0f : NewValue;
+	}
+}
+
+bool UCYAttributeSet::PreGameplayEffectExecute(FGameplayEffectModCallbackData& Data)
+{
+	if (!Super::PreGameplayEffectExecute(Data))
+	{
+		return false;
+	}
+
+	if (Data.EvaluatedData.Attribute == GetDamageAttribute())
+	{
+		if (Data.EvaluatedData.Magnitude > 0.0f)
+		{
+			/*if (Data.Target.HasMatchingGameplayTag(ABTAG_CHARACTER_INVINSIBLE))
+			{
+				Data.EvaluatedData.Magnitude = 0.0f;
+				return false;
+			}*/
+		}
+	}
+
+	return true;
+}
+
+void UCYAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	float MinimumHealth = 0.0f;
+
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), MinimumHealth, GetMaxHealth()));
+	}
+	else if (Data.EvaluatedData.Attribute == GetDamageAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth() - GetDamage(), MinimumHealth, GetMaxHealth()));
+		SetDamage(0.0f);
+	}
+
+	/*if ((GetHealth() <= 0.0f) && !bOutOfHealth)
+	{
+		Data.Target.AddLooseGameplayTag(ABTAG_CHARACTER_ISDEAD);
+		OnOutOfHealth.Broadcast();
+	}
+
+	bOutOfHealth = (GetHealth() <= 0.0f);*/
+}
--- Source/CY/AbilitySystem/Attributes/CYAttributeSet.h
+++ Source/CY/AbilitySystem/Attributes/CYAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "CYAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -14,4 +21,49 @@ class CY_API UCYAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UCYAttributeSet();
+
+	ATTRIBUTE_ACCESSORS(UCYAttributeSet, AttackRange);
+	ATTRIBUTE_ACCESSORS(UCYAttributeSet, MaxAttackRange);
+	ATTRIBUTE_ACCESSORS(UCYAttributeSet, AttackRadius);
+	ATTRIBUTE_ACCESSORS(UCYAttributeSet, MaxAttackRadius);
+	ATTRIBUTE_ACCESSORS(UCYAttributeSet, AttackRate);
+	ATTRIBUTE_ACCESSORS(UCYAttributeSet, MaxAttackRate);
+	ATTRIBUTE_ACCESSORS(UCYAttributeSet, Health);
+	ATTRIBUTE_ACCESSORS(UCYAttributeSet, MaxHealth);
+	ATTRIBUTE_ACCESSORS(UCYAttributeSet, Damage);
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	//virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+	virtual bool PreGameplayEffectExecute(struct FGameplayEffectModCallbackData& Data) override;
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Attack", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData AttackRange;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attack", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxAttackRange;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attack", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData AttackRadius;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attack", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxAttackRadius;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attack", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData AttackRate;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attack", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxAttackRate;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attack", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attack", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData Damage;
 };
